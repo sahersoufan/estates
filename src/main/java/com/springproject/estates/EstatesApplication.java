92016758fd3895d2d255184566c6d53c8d14cd4d
sahersoufan <51461684+sahersoufan@users.noreply.github.com>
2021-11-22 13:51:55 +0200

    Initial commit

---
+++ src/main/java/com/springproject/estates/EstatesApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.estates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springproject/estates/EstatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.estates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
