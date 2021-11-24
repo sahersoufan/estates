428ea04a517069a9b7fd565a78719fc67c63a008
sahersoufan <51461684+sahersoufan@users.noreply.github.com>
2021-11-24 16:01:17 +0200

    20%

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,27 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.scheduling.support.SimpleTriggerContext;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static javax.persistence.FetchType.EAGER;
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity @Data @NoArgsConstructor  @AllArgsConstructor
+public class User {
+
+    @Id @GeneratedValue(strategy = AUTO)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
