ccb83bdce3f8796ff063ed6077ff0760da9ab887
sahersoufan <51461684+sahersoufan@users.noreply.github.com>
2021-11-26 17:13:01 +0200

    JWT 1:11

--- src/main/java/com/springproject/estates/api/UserResource.java
+++ src/main/java/com/springproject/estates/api/UserResource.java
@@ -1,13 +1,16 @@
 package com.springproject.estates.api;
 
+import com.springproject.estates.domain.Role;
 import com.springproject.estates.domain.User;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.config.RepositoryNameSpaceHandler;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.springproject.estates.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,4 +23,30 @@ public class UserResource {
     public ResponseEntity<List<User>> getUsers(){
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+
+    @PostMapping("/user/role")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm roleToUserForm){
+        userService.addRoleToUser(roleToUserForm.getUsername(), roleToUserForm.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+}
+@Data
+class RoleToUserForm {
+
+    private String  username;
+    private String  roleName;
 }
