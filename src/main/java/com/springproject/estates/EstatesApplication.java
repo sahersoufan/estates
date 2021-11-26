0a219ecdb2bb26d14503117676e070436abb8401
sahersoufan <51461684+sahersoufan@users.noreply.github.com>
2021-11-26 17:13:01 +0200

    JWT 1:11

--- src/main/java/com/springproject/estates/EstatesApplication.java
+++ src/main/java/com/springproject/estates/EstatesApplication.java
@@ -1,7 +1,16 @@
 package com.springproject.estates;
 
+import com.springproject.estates.domain.Role;
+import com.springproject.estates.domain.User;
+import com.springproject.estates.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class EstatesApplication {
@@ -10,4 +19,25 @@ public class EstatesApplication {
         SpringApplication.run(EstatesApplication.class, args);
     }
 
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService){
+        return args -> {
+            userService.saveRole(new Role(null,"ROLE_USER"));
+            userService.saveRole(new Role(null,"ROLE_ADMIN"));
+
+            userService.saveUser(new User(null, "jhon jhon", "jhon", "1234",new ArrayList<>()));
+            userService.saveUser(new User(null, "marta marta", "marta", "1234",new ArrayList<>()));
+            userService.saveUser(new User(null, "sali sali", "sali", "1234",new ArrayList<>()));
+
+            userService.addRoleToUser("jhon", "ROLE_USER");
+            userService.addRoleToUser("marta", "ROLE_USER");
+            userService.addRoleToUser("sali", "ROLE_ADMIN");
+        };
+    }
 }
